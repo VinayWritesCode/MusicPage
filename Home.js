0db583d4e99f87352f3cbaa74ba4ffa710ef6260
VinayWritesCode <vinaysuhalka49@gmail.com>
2021-06-26 00:48:46 +0530

    worked on home

--- Home.js
+++ Home.js
@@ -11,3 +11,28 @@ playbtn.onclick = function () {
         playbtn.src = "./resources/images/playbtn.png";
     }
 }
+let data = [
+    { name: 'name0', description: 'description', date: 'XX/XX/XXXX' },
+    { name: 'name1', description: 'description', date: 'XX/XX/XXXX' },
+    { name: 'name2', description: 'description', date: 'XX/XX/XXXX' },
+]
+
+data.forEach(res => {
+    var obj = document.createElement('div');
+    obj.id = "::img";
+    obj.style.cssText = 'width:200px;height:31vh; background-color:transparent; color:#fff;';
+   /*
+    let card = document.createElement("div");
+    card.appendChild(obj);
+    /*let name = document.createTextNode('Name:' + res.name + ', ');
+    card.appendChild(name);
+
+    let description = document.createTextNode('Description:' + res.description + ', ');
+    card.appendChild(description);
+
+    let date = document.createTextNode('date:' + res.date);
+    card.appendChild(date);*/
+    
+    let container = document.querySelector(".album-cards-list");
+    container.appendChild(obj);
+});
